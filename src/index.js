a8e23a15d8485435a6ca2de86bc04c9f03d372e5
G5andeepD <e19063@eng.pdn.ac.lk>
2024-08-18 11:30:44 +0530

    Add cors and swagger docs

--- src/index.js
+++ src/index.js
@@ -2,21 +2,29 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { initializeDb } from './db/database.js';
 import incomeRoutes from './routes/incomeRoutes.js';
 import expenseRoutes from './routes/expenseRoutes.js';
 import summaryRoutes from './routes/summaryRoutes.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Initialize Database
 initializeDb().then(() => {
   console.log('Database initialized');
 });
+
+// Load and Serve OpenAPI spec
+const swaggerDocument = YAML.load('./openapi.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Base Path
 const basePath = '/api/v1';
